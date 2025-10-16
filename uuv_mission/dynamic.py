d38548f28a903de43c0115015a7421c7ffc7d9f1
KateYG529816 <yuan.gao@trinity.ox.ac.uk>
2025-10-17 07:34:34 +0800

    mission loader worked

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import numpy as np
+
+# !!! NEW CODE !!!
+import pandas as pd
+
 import matplotlib.pyplot as plt
 from .terrain import generate_reference_and_limits
 
@@ -76,7 +80,13 @@ class Mission:
     @classmethod
     def from_csv(cls, file_name: str):
         # You are required to implement this method
-        pass
+
+        # !!! NEW CODE !!!
+        df = pd.read_csv(file_name)
+        reference = df['reference'].to_numpy()
+        cave_height = df['cave_height'].to_numpy()
+        cave_depth = df['cave_depth'].to_numpy()
+        return cls(reference, cave_height, cave_depth)
 
 
 class ClosedLoop:
