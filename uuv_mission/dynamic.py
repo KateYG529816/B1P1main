c224265a539a08d438d878339d0fa3c0d8db92ad
KateYG529816 <yuan.gao@trinity.ox.ac.uk>
2025-10-17 08:51:21 +0800

    controller worked

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -106,8 +106,17 @@ class ClosedLoop:
 
         for t in range(T):
             positions[t] = self.plant.get_position()
-            observation_t = self.plant.get_depth()
+
+            # !!! CHANGED CODE !!!
+            current_depth = self.plant.get_depth()
+
             # Call your controller here
+
+            # !!! NEW CODE !!!
+            current_error = mission.reference[t] - current_depth
+            control_action = self.controller.compute_control_action(current_error)
+            actions[t] = control_action
+
             self.plant.transition(actions[t], disturbances[t])
 
         return Trajectory(positions)
